943fc3a895b2f468f0b924269b769774c601db24
quangthangk4 <quangthangkhmt.k22@gmail.com>
2025-03-18 14:42:54 +0700

    secondary commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
-import flowbite from "flowbite-react/tailwind"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    flowbite.plugin(),
-  ],
-
-  content: [
-    flowbite.content(),
   ],
 })
